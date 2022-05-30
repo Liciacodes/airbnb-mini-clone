6f1421f71ffb3e0fc2bc224add2aab4b9cc07e44
Felicia Udosen <udosenfelicia@gmail.com>
2022-05-30 12:58:25 +0100

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <nav>
+      <img src="../images/airbnb 1.png"  alt='logo' className='nav-logo'/>
+    </nav>
+  )
+}
+
+export default Navbar
